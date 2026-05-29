58e6322eddde8bf9e479b0124c47742f6f947609
LucaMiglietta04 <lucamiglietta2004@gmail.com>
2026-05-29 13:33:32 -0300

    Agregado del timing.h y un cambio

--- EjDiapos/EjOMP.c
+++ EjDiapos/EjOMP.c
@@ -2,13 +2,15 @@
 #include <stdio.h>
 
 int main(){
+int id, nt;
+
     #pragma omp parallel
     {
-        int id = omp_get_thread_num();
-        int nt = omp_get_num_threads();
+        id = omp_get_thread_num();
+        nt = omp_get_num_threads();
         printf("id: %d nt: %d\n", id, nt);
     }
     
-//    printf("after parallel id: %d nt: %d\n", id, nt);
+    printf("after parallel id: %d nt: %d\n", id, nt);
     return 0;
 }
